099efe6d075c29a175258ddaadec98d71821e9dd
zhangyuxiang <friedrichzhang02@gmail.com>
2025-09-08 23:41:43 -0400

    after overloading

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,23 @@
 package lab1;
 
 public class Multiples {
+
+    // 有参数版本
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int multiples() {
+        return multiples(1000, 3, 5);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiples());
+    }
 }
--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -1,4 +1,19 @@
 package lab1;
 
 public class Reduce {
+
+    public static int reduce(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        if (sum >= 10) {
+            return reduce(sum);
+        } else {
+            return sum;
+        }
+    }
 }
